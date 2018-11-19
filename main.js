db58d895dc4c0389b0a20eaab55a82a207bef829
LeonardoSarallo <leo.sarallo@gmail.com>
2018-11-19 16:33:35 +0100

    fine esercizio palindromo

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 // Una funzione per capire se la parola è palindroma
 
-var parola = 'osso';
+var parola = 'croce';
 
-var parolapalindroma = [];
+var parolapalindroma = '';
 
-for (var i = parola.lenght; i >= 0; i--) {
-  console.log(i);
+for (var i = parola.length -1; i >= 0; i--)
+{
+
+  parolapalindroma = parolapalindroma + parola[i];
+
+
 }
+
+  if (parolapalindroma == parola) {
+    alert('la parola è palindorma');
+  }
+
+  else {
+    alert('la parola non è palindroma');
+
+  }
